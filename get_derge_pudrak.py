e563ab8bf8423493e13712d289bfc9489f490178
tenzinKunsang <kaldantenzin@gmail.com>
2021-06-23 20:21:10 +0530

    derge transfer done

--- get_derge_pudrak.py
+++ get_derge_pudrak.py
@@ -45,11 +45,11 @@ def save_pagewise(target_pudrak_hfml, vol_num, target_name):
     vol_path = Path(f'./{target_name}_trans_pudrak/{vol_num}/').mkdir(parents=True, exist_ok=True)
     pages = get_pages(target_pudrak_hfml)
     for pg_num, page in enumerate(pages, 1):
-        Path(f'./{target_name}_trans_pudrak/{vol_num}/{pg_num}.txt').write_text(page, encoding="utf-8")
+        Path(f'./{target_name}_trans_pudrak/{vol_num}/{pg_num:04}.txt').write_text(page, encoding="utf-8")
 
 
 def pipeline(target_paths, source_paths, target_name):
-    for target_path, source_path in zip(target_paths, source_paths):
+    for target_path, source_path in zip(target_paths[1:], source_paths[1:]):
         print(f'INFO: {source_path.stem} tranfering line break to {target_path.stem}..')
         target_hfml = target_path.read_text(encoding='utf-8')
         source_hfml = source_path.read_text(encoding='utf-8')
